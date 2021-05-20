0c50466baabc51b7c2e743300c745bc8a540c037
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-05-20 11:29:12 +0300

    entry and output config

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
+module.exports = {
+  entry: {
+    //nurodom pagrindini js faila
+    main: path.resolve(__dirname, "./src/app.js"),
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [],
+};
