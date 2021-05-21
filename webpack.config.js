f6cd786923c805279227ef1af5d81744f4010303
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-05-21 09:25:36 +0300

    added babel , works like charm

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  devtool: false, // kai sukompiliuojam nebutu nesamoniu nereikalingu
   entry: {
     //nurodom pagrindini js faila
     main: path.resolve(__dirname, "./src/app.js"),
@@ -11,7 +12,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
