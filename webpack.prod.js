ee5f17c18919d92c7e73d51c1f30a8af7b61afe7
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-05-24 09:51:01 +0300

    browserrc added

--- webpack.prod.js
+++ webpack.prod.js
@@ -22,7 +22,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
